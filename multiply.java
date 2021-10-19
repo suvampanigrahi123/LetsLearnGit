409a27625ec8c4716fbf729d520f02236c574a34
suvam panigrahi <suvampanigrahi143@gmail.com>
2021-10-19 17:03:18 +0530

    changed some code

--- multiply.java
+++ multiply.java
@@ -1,6 +1,6 @@
 public class multiply {
     public static void main(String[] args) {
         int a=10,b=8;
-        System.out.println("Prroduct is "+a*b);
+        System.out.println("Prroduct of two number "+a*b);
     }
 }
