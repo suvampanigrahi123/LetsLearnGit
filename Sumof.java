86cd01a441c79aedc2edf22158d5fb7fd1ddd9b1
suvam panigrahi <suvampanigrahi143@gmail.com>
2021-10-19 12:37:02 +0530

    intial commit

---
+++ Sumof.java
@@ -0,0 +1,12 @@
+
+class SumOfjava{
+    public static void main(String[] args) {
+        int a=10;
+        int b=12;
+        int ans=sum(a,b);
+        System.out.println(ans);
+    }
+    static int sum(int a,int b){
+        return a+b;
+    }
+}
